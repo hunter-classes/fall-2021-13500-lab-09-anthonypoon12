bbda87b53f868cae186fb065ec6296f00e486faf
anthonypoon12 <anthony@poons.us>
2021-11-06 13:32:11 -0400

    added farthestFromOrigin and test case

--- funcs.cpp
+++ funcs.cpp
@@ -4,3 +4,8 @@
 double length(Coord3D *p){
     return sqrt(pow(p->x,2)+pow(p->y,2)+pow(p->z,2));
 }
+Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
+    if (length(p1)>length(p2))
+        return p1;
+    return p2;
+}
--- tests.cpp
+++ tests.cpp
@@ -1,7 +1,12 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "funcs.h"
-Coord3D pointP = {10, 20, 30};
+Coord3D pointP = {10, 20, 30};  
+Coord3D pointP1 = {10, 20, 30};
+Coord3D pointQ = {-20, 21, -22};
 TEST_CASE("lengths"){
     CHECK(length(&pointP)-37.4166<0.00001);
+}
+TEST_CASE("farthest"){
+    CHECK(fartherFromOrigin(&pointP1, &pointQ)==&pointP1);
 }
